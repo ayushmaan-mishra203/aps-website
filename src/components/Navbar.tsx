0338a2be4c0dda292451ceaf4183f8c7a6c1028a
ayushmaan-mishra203 <142373193+ayushmaan-mishra203@users.noreply.github.com>
2025-08-18 10:02:21 +0530

    First commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='p-8'>
+        <div className='flex items-center'>
+            <div>
+                <Link href='/' className='text-3xl font-bold text-gray-800'>APS Technologies</Link>
+            </div>
+            <div className='ml-auto flex space-x-4 gap-14 text-lg'>
+                <Link href='/' className=' font-bold text-gray-800'>Home</Link>
+                <Link href='/about' className=' font-bold text-gray-800'>About</Link>
+                <Link href='/services' className=' font-bold text-gray-800'>Services</Link>
+                <Link href='/contact' className=' font-bold text-gray-800'>Contact</Link>
+                <Link href='/blogs' className=' font-bold text-gray-800'>Blogs</Link>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
